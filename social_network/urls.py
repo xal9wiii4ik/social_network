1e65a49a103b755c2b28e3b026b27762445a3185
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2021-03-12 13:12:38 +0300

    some change

--- social_network/urls.py
+++ social_network/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
 from rest_framework.routers import SimpleRouter
@@ -10,12 +11,12 @@ from apps.userprofile.views import (
 )
 from apps.followers.views import (
     FollowerModelViewSet,
-    SubscriberModelViewSet,
 )
 from apps.post.views import (
     SubjectModelViewSet,
     PostModelViewSet,
     CommentModelViewSet,
+    LikeDislikeApiView,
 )
 from apps.auth_user.views import (
     RegistrationView,
@@ -24,12 +25,12 @@ from apps.auth_user.views import (
     ResetPasswordView,
     SetNewPasswordView,
 )
+from social_network import settings
 
 router = SimpleRouter()
 router.register(r'user_profile', AbstractUserOwnerViewSet)
-router.register(r'guest_user_profile', AbstractUserGuestsViewSet)
+router.register(r'guest_user_profile', AbstractUserGuestsViewSet, basename='guestcustomuser')
 router.register(r'followers', FollowerModelViewSet)
-router.register(r'subscribers', SubscriberModelViewSet)
 router.register(r'posts', PostModelViewSet)
 router.register(r'subjects', SubjectModelViewSet)
 router.register(r'comments', CommentModelViewSet)
@@ -44,6 +45,9 @@ urlpatterns = [
     path('auth/login/', LogInView.as_view(), name='login'),
     path('auth/reset_password/', ResetPasswordView.as_view(), name='reset_password'),
     path('auth/reset_password/<str:uid>/<str:token>/', SetNewPasswordView.as_view(), name='reset_password_confirm'),
+
+    path('likedislike/<str:post_pk>/', LikeDislikeApiView.as_view(), name='post_likes_dislikes'),
 ]
 
 urlpatterns += router.urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
